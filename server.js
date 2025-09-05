fc51694809093bb6dcd7f8b77eaa3b730f83e7b9
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-05 13:24:20 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Collaborative Kaizen API!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
