454622f3914e8eb373dafb0dbcf1ad8991e3aa07
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-05 13:25:27 -0500

    Update port number to use environment variable or default to 3000

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'Healthy' });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
